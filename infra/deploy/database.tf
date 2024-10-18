aafb203ad35ed3f08a677658d02253b569005959
shivam <shivam086r@gmail.com>
2024-10-18 11:02:08 +0530

    updated ecs SG

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "rds" {
     from_port = 5432
     to_port   = 5432
 
-     security_groups = [
+    security_groups = [
       aws_security_group.ecs_service.id
     ]
   }

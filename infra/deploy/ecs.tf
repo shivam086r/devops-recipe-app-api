58fc6af440706c2688f330a1ff33ad8748b1b063
shivam <shivam086r@gmail.com>
2024-10-18 10:17:00 +0530

    added security group for ecs

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -127,11 +127,6 @@ resource "aws_ecs_task_definition" "api" {
             containerPath = "/vol/static"
             sourceVolume  = "static"
           },
-          {
-            readOnly      = true
-            containerPath = "/vol/media"
-            sourceVolume  = "efs-media"
-          }
         ]
         logConfiguration = {
           logDriver = "awslogs"
@@ -152,5 +147,40 @@ resource "aws_ecs_task_definition" "api" {
   runtime_platform {
     operating_system_family = "LINUX"
     cpu_architecture        = "X86_64"
+  }
+}
+
+resource "aws_security_group" "ecs_service" {
+  description = "Access rules for the ECS service."
+  name        = "${local.prefix}-ecs-service"
+  vpc_id      = aws_vpc.main.id
+
+  # Outbound access to endpoints
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # RDS connectivity
+  egress {
+    from_port = 5432
+    to_port   = 5432
+    protocol  = "tcp"
+    cidr_blocks = [
+      aws_subnet.private_a.cidr_block,
+      aws_subnet.private_b.cidr_block,
+    ]
+  }
+
+  # HTTP inbound access
+  ingress {
+    from_port = 8000
+    to_port   = 8000
+    protocol  = "tcp"
+    security_groups = [
+      aws_security_group.lb.id
+    ]
   }
 }

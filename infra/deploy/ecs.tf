878aca1a158b50dbf578dd14835fce067cf8ba5a
shivam <shivam086r@gmail.com>
2024-10-19 02:18:47 +0530

    fix ecs.tf

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -174,10 +174,10 @@ resource "aws_security_group" "ecs_service" {
 
   # HTTP inbound access
   ingress {
-    from_port       = 8000
-    to_port         = 8000
-    protocol        = "tcp"
-    security_groups = ["0.0.0.0/0"]
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
